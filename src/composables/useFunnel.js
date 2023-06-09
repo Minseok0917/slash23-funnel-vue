6172cd3eb8839c04245973b71cbd1378a2c5bca4
Minseok Jung <37059223+Minseok0917@users.noreply.github.com>
2023-06-09 17:08:40 +0900

    refactoring : useFunnel 추가

---
+++ src/composables/useFunnel.js
@@ -0,0 +1,25 @@
+import { useState } from "./useState";
+import { computed, watch } from "vue";
+import { useRoute, useRouter } from "vue-router";
+
+export function useFunnel({ stepName, steps, state: initialState }) {
+    const route = useRoute();
+    const router = useRouter();
+    const [step, setStep] = useState(route.query.step || stepName);
+    const [state, setState] = useState(initialState);
+    const routeQueryStep = computed(() => route.query.step);
+
+    function updateQueryStep(stepName) {
+        setStep(stepName);
+        router.push({ query: { step: stepName } });
+    }
+
+    watch(routeQueryStep, setStep);
+    router.replace({ query: { step: step.value } });
+
+    return {
+        step,
+        state,
+        steps: steps({ setState, setStep: updateQueryStep }),
+    };
+}
--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -4,52 +4,39 @@
     </section>
 </template>
 <script setup>
-import { useState } from "@composables/useState";
-import { watch, computed } from "vue";
-import { useRouter, useRoute } from "vue-router";
+import { useFunnel } from "@composables/useFunnel";
 import Funnel from "@components/Funnel.vue";
 import RegisterName from "./RegisterName.vue";
 import RegisterAddress from "./RegisterAddress.vue";
 import RegisterSuccess from "./RegisterSuccess.vue";
 
-const route = useRoute();
-const router = useRouter();
-const [step, setStep] = useState("name");
-const [state, setState] = useState({
-    name: "",
-    address: "",
+const { step, steps, state } = useFunnel({
+    stepName: "name",
+    state: {
+        name: "",
+        address: "",
+    },
+    steps: ({ setStep, setState }) => [
+        {
+            stepName: "name",
+            component: RegisterName,
+            onStep({ name }) {
+                setState({ ...state.value, name });
+                setStep("address");
+            },
+        },
+        {
+            stepName: "address",
+            component: RegisterAddress,
+            onStep({ address }) {
+                setState({ ...state.value, address });
+                setStep("success");
+            },
+        },
+        {
+            stepName: "success",
+            component: RegisterSuccess,
+        },
+    ],
 });
-
-function setRouteStep(stepName) {
-    setStep(stepName);
-    router.push({ query: { step: stepName } });
-}
-
-const queryStep = computed(() => route.query.step);
-watch(queryStep, setStep);
-!queryStep.value && router.replace({ query: { step: step.value } });
-queryStep.value && setStep(queryStep.value);
-
-const steps = [
-    {
-        stepName: "name",
-        component: RegisterName,
-        onStep({ name }) {
-            setState({ ...state.value, name });
-            setRouteStep("address");
-        },
-    },
-    {
-        stepName: "address",
-        component: RegisterAddress,
-        onStep({ address }) {
-            setState({ ...state.value, address });
-            setRouteStep("success");
-        },
-    },
-    {
-        stepName: "success",
-        component: RegisterSuccess,
-    },
-];
 </script>
