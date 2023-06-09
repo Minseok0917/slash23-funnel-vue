2279ab21483e9eee746cfda70f6bc30236f257ca
Minseok Jung <37059223+Minseok0917@users.noreply.github.com>
2023-06-09 18:06:06 +0900

    feat : funnel mermaid 적용 진행중

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,14 +1,31 @@
 <template>
     <section class="register">
         <Funnel :step="step" :steps="steps" :state="state" />
+        <pre class="mermaid">{{ m }}</pre>
     </section>
 </template>
 <script setup>
 import { Funnel, useFunnel } from "@functions/funnel";
+import { onMounted } from "vue";
 import RegisterName from "./RegisterName.vue";
 import RegisterAddress from "./RegisterAddress.vue";
 import RegisterSuccess from "./RegisterSuccess.vue";
+import mermaid from "mermaid";
 
+const m = `
+    graphe TD
+        A[Enter Chart Definition] --> B(Preview)
+        B --> C{decide}
+        C --> D[Keep]
+        C --> E[Edit Definition]
+        E --> B
+        D --> F[Save Image and Code]
+        F --> B 
+`;
+
+// onMounted(() => {
+mermaid.initialize({ startOnLoad: true });
+// });
 const { step, steps, state } = useFunnel({
     stepName: "name",
     state: {
